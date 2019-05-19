199687cbcd719b7cf2f9ccd524b1f00e1cb0a53c
kevin lang <langkevina@gmail.com>
2019-05-19 19:22:39 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -5,12 +5,8 @@ def using_times
   end
 end
 
-# 3.times do 
+# 3.times do
 #   puts "Hiding 10 stolen jewels."
 #   jewels_in_bag = jewels_in_bag - 10
 #   puts "Now there are only #{jewels_in_bag} jewels left to hide!"
 # end
-
-
-
-
